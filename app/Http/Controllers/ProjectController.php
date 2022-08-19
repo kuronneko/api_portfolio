28243548bf5d554561d91cbbf9bca45845c7d700
ubuntudesktop <ubuntudesktop@ubuntudesktop.com>
2022-08-18 23:37:56 -0400

    projects show, status and delete

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detail;
+use Illuminate\Http\Request;
+use App\Models\Persona;
+use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::with(['detail'])->where('persona_id', Auth::user()->id)->get();
+        return response()->json($projects);
+    }
+
+         /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function status(Project $project)
+    {
+        if($project->status == 1){
+            $project->status = 0;
+        }else{
+            $project->status = 1;
+        }
+        $project->save();
+        return response()->json([
+            'project' => $project,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        Detail::where('project_id', $project->id)->delete();
+        $project->delete();
+        return response()->json([
+            'mensaje' => 'Project deleted',
+        ]);
+    }
+}
