ff3c8906fd300aeeb334399bde845e7dbbb4baae
wdocker2 <wdocker2@wdocker2.com>
2022-10-10 17:40:32 -0300

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>PortfolioGestor9000</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
