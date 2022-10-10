f8d9c7499c20287b36dd5a61a903b4a7c3bd413a
wdocker2 <wdocker2@wdocker2.com>
2022-10-10 06:16:32 -0300

    image background opacity

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,40 +1,50 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="shortcut icon" type="image/x-icon" href="/img/logo.png" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" type="image/x-icon" href="/img/logo.png" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-        <style>
-            @media only screen and (min-width: 600px) {
-                body{
-                    background-image: url(../img/toja.png);
-                    background-repeat: no-repeat;
-                    background-attachment: fixed;
-                    background-position: right -300px bottom -300px;
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <style>
+        @media only screen and (min-width: 600px) {
+            body::after {
+                content: "";
+                background-image: url(../img/toja.png);
+                background-repeat: no-repeat;
+                background-position: right -300px bottom -300px;
+                opacity: 0.5;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                right: 0;
+                position: fixed;
+                z-index: -1;
             }
-            }
-        </style>
-    </head>
+        }
+    </style>
+</head>
 
-    <body class="bg-dark">
-        <div class="container mt-5 mb-3 py-4" id="app">
+<body class="bg-dark">
+    <div class="container mt-5 mb-3 py-4" id="app">
 
-        </div>
-        <footer class="fixed-bottom bg-dark text-center text-white">PortfolioGestor9000 developed by <a class="text-success text-decoration-none" href="https://kuronneko.github.io">kuronneko</a></footer>
+    </div>
+    <footer class="fixed-bottom bg-dark text-center text-white">PortfolioGestor9000 developed by <a
+            class="text-success text-decoration-none" href="https://kuronneko.github.io">kuronneko</a></footer>
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
-    </body>
+</body>
+
 </html>
